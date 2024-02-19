c7e9a06efaaecc141423287f839722b2d3505ced
EthanMiller758 <138612628+EthanMiller758@users.noreply.github.com>
2024-02-19 12:12:11 -0330

    Delete FitnessClass.sql

--- FitnessClass.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE FitnessClass (
-    class_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    duration INTERVAL,
-    schedule VARCHAR(100),
-    capacity INT
-);
