0a0f608b98b42fb320e8641dacf2088e4bd02a94
EthanMiller758 <138612628+EthanMiller758@users.noreply.github.com>
2024-02-26 19:27:29 -0330

    Add files via upload

---
+++ log.js
@@ -0,0 +1,18 @@
+// Inside the CLI script
+
+const fs = require('fs');
+
+function logAction(actionDescription, username) {
+    const timeStamp = new Date().toISOString();
+    const logEntry = `${timeStamp} - ${actionDescription} by ${username}\n`;
+
+    fs.appendFile('app-events.log', logEntry, 'utf8', err => {
+        if (err) {
+            console.error('Error writing to log file:', err);
+        }
+    });
+}
+
+module.exports = {
+    logAction
+}
